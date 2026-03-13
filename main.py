e66d9b710f39a15b003f82589fb88b7e55e32fd5
RaymondHub-Lgtm <raymondismael@icloud.com>
2026-03-13 02:26:25 +0100

    agregue el menu de inicio

--- main.py
+++ main.py
@@ -1,8 +1,70 @@
-from cliente import Cliente
+import tkinter as tk
 
-def main():
-    cliente = Cliente("Juan")
-    print("Cliente creado:", cliente.nombre)
+def abrir_clientes():
+    print("Abrir modulo de clientes")
 
-if __name__ == "__main__":
-    main()
+def abrir_productos():
+    print("Abrir modulo de productos")
+
+def abrir_inventario():
+    print("Abrir modulo de inventario")
+
+def abrir_facturas():
+    print("Abrir modulo de facturas")
+
+ventana = tk.Tk()
+ventana.title("Sistema de Facturacion")
+ventana.geometry("600x400")
+ventana.config(bg="#2c3e50")
+
+titulo = tk.Label(
+    ventana,
+    text="Sistema de Facturacion",
+    font=("Arial",22, "bold"),
+    bg="#2c3e50",
+    fg="White"
+)
+
+boton_clientes = tk.Button(
+    ventana,
+    text="Clientes",
+    width=20,
+    height=2,
+    command=abrir_clientes
+
+)
+
+boton_clientes.pack(pady=10)
+
+boton_productos = tk.Button(
+    ventana,
+    text="Inventario",
+    width=20,
+    height=2,
+    command=abrir_productos
+
+)
+
+boton_productos.pack(pady=10)
+
+boton_inventario = tk.Button(
+    ventana,
+    text="Inventario",
+    width=20,
+    height=2,
+    command=abrir_inventario
+)
+
+boton_inventario.pack(pady=10)
+
+boton_facturas = tk.Button(
+    ventana,
+    text="Facturas",
+    width=20,
+    height=2,
+    command=abrir_facturas
+)
+
+boton_facturas.pack(pady=10)
+
+ventana.mainloop()
